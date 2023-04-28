bf770a7509af5da8e1034ef9206cd3562b8bddb3
michaelbuff <michael.t.buffington@gmail.com>
2023-04-28 10:08:18 -0600

    fixed merges for buff and other's changes

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -38,6 +38,7 @@ function App() {
         {
           path: "checkin/:raceId/:date",
           element: <CheckInScreen />,
+          loader: CheckinLoader,
         },
         {
           path: "adminlogin",
